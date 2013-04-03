a1892f0a59d76d31a7447c8f487079f5d43580cb
Jacob Jennings <jacob.r.jennings@gmail.com>
2013-04-02 23:57:39 -0700

    Add example for CI processing

--- HTCoreImageDemo/HTCoreImageDemo/HTViewController.m
+++ HTCoreImageDemo/HTCoreImageDemo/HTViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "HTViewController.h"
+#import "HTCoreImage.h"
+#import "CIFilter+HTCICategoryColorAdjustment.h"
 
 @interface HTViewController ()
 
@@ -18,6 +20,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    UIImage *someImage = [UIImage imageNamed:@"asdf"];
+
+    CIImage *filteredImage = [[someImage toCIImage] imageByApplyingFilters:@[
+        [CIFilter filterColorControlsSaturation:1 brightness:1 contrast:1],
+        [CIFilter filterColorMatrixWithAlpha:0.5]
+    ]];
+
+    [filteredImage processToUIImageCompletion:^(UIImage *uiImage)
+    {
+        NSLog(@"%@", uiImage);
+    }];
 }
 
 - (void)didReceiveMemoryWarning
