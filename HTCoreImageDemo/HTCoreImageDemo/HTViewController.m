8b859fb617e82eeaf9079cdc9adb55f5daaa4412
Jacob Jennings <jacob.r.jennings@gmail.com>
2013-04-03 09:45:11 -0700

    Add before/after example for HTCoreImage

--- HTCoreImageDemo/HTCoreImageDemo/HTViewController.m
+++ HTCoreImageDemo/HTCoreImageDemo/HTViewController.m
@@ -21,15 +21,55 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-    UIImage *someImage = [UIImage imageNamed:@"asdf"];
+    
+    
+    UIImage *sourceUIImage = [UIImage imageNamed:@"asdf"];
+    
+    
+    
+    // Native application of color controls (increase contrast) + color matrix alpha (blueify)
+    
+    CIImage *sourceCIImage = [CIImage imageWithCGImage:sourceUIImage.CGImage];
 
-    CIImage *filteredImage = [[someImage toCIImage] imageByApplyingFilters:@[
-        [CIFilter filterColorControlsSaturation:1 brightness:1 contrast:1],
-        [CIFilter filterColorMatrixWithAlpha:0.5]
-    ]];
-
-    [filteredImage processToUIImageCompletion:^(UIImage *uiImage)
-    {
+    CIFilter *colorControlsFilter = [CIFilter filterWithName:@"CIColorControls"];
+    [colorControlsFilter setDefaults];
+    [colorControlsFilter setValue:@(1) forKey:@"inputSaturation"];
+    [colorControlsFilter setValue:@(1) forKey:@"inputBrightness"];
+    [colorControlsFilter setValue:@(2) forKey:@"inputContrast"];
+    [colorControlsFilter setInputCIImage:sourceCIImage];
+    
+    CIFilter *colorMatrixFilter = [CIFilter filterWithName:@"CIColorMatrix"];
+    [colorMatrixFilter setDefaults];
+    [colorMatrixFilter setValue:[CIVector vectorWithX:0.5 Y:0 Z:0 W:0]
+                         forKey:@"inputRVector"];
+    [colorMatrixFilter setValue:[CIVector vectorWithX:0 Y:0.5 Z:0 W:0]
+                         forKey:@"inputGVector"];
+    [colorMatrixFilter setValue:[CIVector vectorWithX:0 Y:0 Z:1 W:0]
+                         forKey:@"inputBVector"];
+    [colorMatrixFilter setValue:[CIVector vectorWithX:0 Y:0 Z:0 W:1]
+                         forKey:@"inputAVector"];
+    [colorMatrixFilter setInputCIImage:[colorControlsFilter outputImage]];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^
+                   {
+                       CIContext *ciContext = [CIContext contextWithOptions:@{ kCIContextUseSoftwareRenderer : (id)kCFBooleanFalse }];
+                       CGImageRef resultCGImage = [ciContext createCGImage:[colorMatrixFilter outputImage] fromRect:[[colorMatrixFilter outputImage] extent]];
+                       UIImage *resultUIImage = [UIImage imageWithCGImage:resultCGImage scale:sourceUIImage.scale orientation:sourceUIImage.imageOrientation];
+                       CGImageRelease(resultCGImage);
+                       dispatch_async(dispatch_get_main_queue(), ^
+                                      {
+                                          NSLog(@"%@", resultUIImage);
+                                      });
+                   });
+    
+    
+    
+    // Same application using convenience categories in HTCoreImage
+    
+    [[[sourceUIImage toCIImage] imageByApplyingFilters:@[
+      [CIFilter filterColorControlsSaturation:1 brightness:1 contrast:1],
+      [CIFilter filterColorMatrixWithRed:0.5 green:0.5 blue:1 alpha:1]]]
+     processToUIImageCompletion:^(UIImage *uiImage) {
         NSLog(@"%@", uiImage);
     }];
 }
