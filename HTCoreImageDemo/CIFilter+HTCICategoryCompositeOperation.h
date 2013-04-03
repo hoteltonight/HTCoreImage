f5ff2d4c4b87f3cbb03718c51daf573c7435bf2f
Jacob Jennings <jacob.r.jennings@gmail.com>
2013-04-02 23:51:18 -0700

    CIColorCategoryCompositeOperation constructors added

--- HTCoreImageDemo/CIFilter+HTCICategoryCompositeOperation.h
+++ HTCoreImageDemo/CIFilter+HTCICategoryCompositeOperation.h
@@ -10,4 +10,50 @@
 
 @interface CIFilter (HTCICategoryCompositeOperation)
 
++ (CIFilter *)filterAdditionCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterColorBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterColorBurnBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterColorDodgeBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterDarkenBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterDifferenceBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterExclusionBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterHardLightBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterHueBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterLightenBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterLuminosityBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterMaximumCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterMinimumCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterMultiplyBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterMultiplyCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterOverlayBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterSaturationBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterScreenBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterSoftLightBlendModeWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterSourceAtopCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterSourceInCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterSourceOutCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(6_0);
+
++ (CIFilter *)filterSourceOverCompositingWithBackgroundImage:(CIImage *)backgroundImage NS_AVAILABLE_IOS(5_0);
+
 @end
--- HTCoreImageDemo/CIFilter+HTCICategoryCompositeOperation.m
+++ HTCoreImageDemo/CIFilter+HTCICategoryCompositeOperation.m
@@ -10,4 +10,189 @@
 
 @implementation CIFilter (HTCICategoryCompositeOperation)
 
++ (CIFilter *)filterAdditionCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *additionCompositingFilter = [CIFilter filterWithName:@"CIAdditionCompositing"];
+    [additionCompositingFilter setDefaults];
+    [additionCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return additionCompositingFilter;
+}
+
++ (CIFilter *)filterColorBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *colorBlendFilter = [CIFilter filterWithName:@"CIColorBlendMode"];
+    [colorBlendFilter setDefaults];
+    [colorBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return colorBlendFilter;
+}
+
++ (CIFilter *)filterColorBurnBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *colorBurnBlendFilter = [CIFilter filterWithName:@"CIColorBurnBlendMode"];
+    [colorBurnBlendFilter setDefaults];
+    [colorBurnBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return colorBurnBlendFilter;
+}
+
++ (CIFilter *)filterColorDodgeBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *colorDodgeBlendFilter = [CIFilter filterWithName:@"CIColorDodgeBlendMode"];
+    [colorDodgeBlendFilter setDefaults];
+    [colorDodgeBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return colorDodgeBlendFilter;
+}
+
++ (CIFilter *)filterDarkenBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *darkenBlendFilter = [CIFilter filterWithName:@"CIDarkenBlendMode"];
+    [darkenBlendFilter setDefaults];
+    [darkenBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return darkenBlendFilter;
+}
+
++ (CIFilter *)filterDifferenceBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *differenceBlendFilter = [CIFilter filterWithName:@"CIDifferenceBlendMode"];
+    [differenceBlendFilter setDefaults];
+    [differenceBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return differenceBlendFilter;
+}
+
++ (CIFilter *)filterExclusionBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *exclusionBlendFilter = [CIFilter filterWithName:@"CIExclusionBlendMode"];
+    [exclusionBlendFilter setDefaults];
+    [exclusionBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return exclusionBlendFilter;
+}
+
++ (CIFilter *)filterHardLightBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *hardLightBlendFilter = [CIFilter filterWithName:@"CIHardLightBlendMode"];
+    [hardLightBlendFilter setDefaults];
+    [hardLightBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return hardLightBlendFilter;
+}
+
++ (CIFilter *)filterHueBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *hueBlendFilter = [CIFilter filterWithName:@"CIHueBlendMode"];
+    [hueBlendFilter setDefaults];
+    [hueBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return hueBlendFilter;
+}
+
++ (CIFilter *)filterLightenBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *lightenBlendFilter = [CIFilter filterWithName:@"CILightenBlendMode"];
+    [lightenBlendFilter setDefaults];
+    [lightenBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return lightenBlendFilter;
+}
+
++ (CIFilter *)filterLuminosityBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *luminosityBlendFilter = [CIFilter filterWithName:@"CILuminosityBlendMode"];
+    [luminosityBlendFilter setDefaults];
+    [luminosityBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return luminosityBlendFilter;
+}
+
++ (CIFilter *)filterMaximumCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *maximumCompositingFilter = [CIFilter filterWithName:@"CIMaximumCompositing"];
+    [maximumCompositingFilter setDefaults];
+    [maximumCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return maximumCompositingFilter;
+}
+
++ (CIFilter *)filterMinimumCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *minimumCompositingFilter = [CIFilter filterWithName:@"CIMinimumCompositing"];
+    [minimumCompositingFilter setDefaults];
+    [minimumCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return minimumCompositingFilter;
+}
+
++ (CIFilter *)filterMultiplyBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *multiplyBlendFilter = [CIFilter filterWithName:@"CIMultiplyBlendMode"];
+    [multiplyBlendFilter setDefaults];
+    [multiplyBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return multiplyBlendFilter;
+}
+
++ (CIFilter *)filterMultiplyCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *multiplyCompositingFilter = [CIFilter filterWithName:@"CIMultiplyCompositing"];
+    [multiplyCompositingFilter setDefaults];
+    [multiplyCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return multiplyCompositingFilter;
+}
+
++ (CIFilter *)filterOverlayBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *overlayBlendFilter = [CIFilter filterWithName:@"CIOverlayBlendMode"];
+    [overlayBlendFilter setDefaults];
+    [overlayBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return overlayBlendFilter;
+}
+
++ (CIFilter *)filterSaturationBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *saturationBlendFilter = [CIFilter filterWithName:@"CISaturationBlendMode"];
+    [saturationBlendFilter setDefaults];
+    [saturationBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return saturationBlendFilter;
+}
+
++ (CIFilter *)filterScreenBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *screenBlendFilter = [CIFilter filterWithName:@"CIScreenBlendMode"];
+    [screenBlendFilter setDefaults];
+    [screenBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return screenBlendFilter;
+}
+
++ (CIFilter *)filterSoftLightBlendModeWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *softLightBlendFilter = [CIFilter filterWithName:@"CISoftLightBlendMode"];
+    [softLightBlendFilter setDefaults];
+    [softLightBlendFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return softLightBlendFilter;
+}
+
++ (CIFilter *)filterSourceAtopCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *sourceAtopCompositingFilter = [CIFilter filterWithName:@"CISourceAtopCompositing"];
+    [sourceAtopCompositingFilter setDefaults];
+    [sourceAtopCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return sourceAtopCompositingFilter;
+}
+
++ (CIFilter *)filterSourceInCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *sourceInCompositingFilter = [CIFilter filterWithName:@"CISourceInCompositing"];
+    [sourceInCompositingFilter setDefaults];
+    [sourceInCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return sourceInCompositingFilter;
+}
+
++ (CIFilter *)filterSourceOutCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *sourceOutCompositingFilter = [CIFilter filterWithName:@"CISourceOutCompositing"];
+    [sourceOutCompositingFilter setDefaults];
+    [sourceOutCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return sourceOutCompositingFilter;
+}
+
++ (CIFilter *)filterSourceOverCompositingWithBackgroundImage:(CIImage *)backgroundImage
+{
+    CIFilter *sourceOverCompositingFilter = [CIFilter filterWithName:@"CISourceOverCompositing"];
+    [sourceOverCompositingFilter setDefaults];
+    [sourceOverCompositingFilter setValue:backgroundImage forKey:@"inputBackgroundImage"];
+    return sourceOverCompositingFilter;
+}
+
+
 @end
